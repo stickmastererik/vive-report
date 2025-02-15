b6f4001c13970098abf3a59072af848729aca762
Oat <bendawsdev@gmail.com>
2025-02-14 22:50:15 -0600

    Fixed the black screen bug

--- src/OculusReportMenu.cs
+++ src/OculusReportMenu.cs
@@ -1,4 +1,5 @@
 using BepInEx;
+using HarmonyLib;
 using System;
 using System.Reflection;
 using UnityEngine;
@@ -7,27 +8,28 @@ namespace OculusReportMenu {
     [BepInPlugin("org.oatsalmon.gorillatag.oculusreportmenu", "Oculus Report Menu", "1.1.0")]
     public class Plugin : BaseUnityPlugin
     {
-        GorillaMetaReport gr = GameObject.Find("Miscellaneous Scripts").transform.Find("MetaReporting").GetComponent<GorillaMetaReport>();
-        static bool MenuOpen = false;
         static bool ModEnabled = true;
+        public static bool Menu = false;
+        static float debounceTime = 0.0f;
 
         public void Update()
         {
+            if (!ModEnabled) return;
+
             if (ControllerInputPoller.instance.leftControllerSecondaryButton)
             {
-                if (!MenuOpen && ModEnabled)
+                if (!Menu && Time.time >= debounceTime) // stop it from opening the menu a whole ton
                 {
-                    Menus.OculusReportMenu();
-                    MenuOpen = true;
-                }
-            } else
-            {
-                MenuOpen = false;
+                    debounceTime = Time.time + 1; // for some reason, just opening it too fast after another thing makes it black out your game until you restart
+                                                  // this should hopefully fix that issue
 
-                gr.gameObject.SetActive(false);
-                gr.enabled = false;
-                MethodInfo inf = typeof(GorillaMetaReport).GetMethod("StopOverlay", BindingFlags.NonPublic | BindingFlags.Instance);
-                inf.Invoke(gr, null);
+                    GorillaMetaReport gr = GameObject.Find("Miscellaneous Scripts").transform.Find("MetaReporting").GetComponent<GorillaMetaReport>();
+                    gr.gameObject.SetActive(true);
+                    Menu = true;
+                    gr.enabled = true;
+                    MethodInfo inf = typeof(GorillaMetaReport).GetMethod("StartOverlay", BindingFlags.NonPublic | BindingFlags.Instance);
+                    inf.Invoke(gr, null);
+                }
             }
         }
 
@@ -39,6 +41,15 @@ namespace OculusReportMenu {
         public void OnDisable() {
             HarmonyPatches.RemoveHarmonyPatches();
             ModEnabled = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(GorillaMetaReport), "Teardown")] // GorillaMetaReport.Teardown() is called when X is pressed
+    public class CheckMenuClosed
+    {
+        static void Postfix()
+        {
+            Plugin.Menu = false;
         }
     }
 }
